8021bcacdda9fe9105423c481845f391f25e57c9
Kieran <kieran@keywebdesigns.com>
2017-12-12 08:23:24 -0800

    update to laravel 5.5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Plans
+                    Plans Connect
                 </div>
 
             </div>
